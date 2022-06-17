0b5f81aa9edb0103d121728fa1bf312bacd4c8a6
Alexander <sashashvetsov3@gmail.com>
2022-06-17 21:02:20 +0300

    fix bug

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,17 +3,13 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import styles from './app.module.css'
-import Modal from '../modal/modal';
-import OrderDetails from '../order-details/order-details';
-import IngredientDetails from '../ingredient-details/ingredient-details';
 import { BurgerContext } from '../../services/burger-context';
 import { getIngredients } from '../../utils/api';
 
 
 function App() {
-  const [isIngredientDetails, setIsIngredientDetails] = React.useState(false);
   const [data, setData] = useState([]);
-  const [id, setId] = React.useState('')
+
 
   useEffect(() => {
     getIngredients()
@@ -21,42 +17,15 @@ function App() {
       .catch(error => console.error(error))
   }, [])
 
-
-  // Закрытие всех модалок
-  const closeAllModals = () => {
-    setIsIngredientDetails(false)
-  };
-
-  const openIngredientModal = (id) => {
-    setIsIngredientDetails(true)
-    setId(id)
-  }
-
-  // Обработка нажатия Esc
-  const handleEscKeydown = (e) => {
-    e.key === "Escape" && closeAllModals();
-  };
-
-
   return (
     <div className={styles.page}>
       <AppHeader />
       <main className={styles.main}>
         <BurgerContext.Provider value={data}>
-          <BurgerIngredients openIngredientModal={openIngredientModal} />
+          <BurgerIngredients />
           <BurgerConstructor />
         </BurgerContext.Provider>
       </main>
-
-      {isIngredientDetails &&
-        <Modal
-          title="Детали ингредиента"
-          onOverlayClick={closeAllModals}
-          onEscKeydown={handleEscKeydown}
-        >
-          <IngredientDetails data={data} id={id} />
-        </Modal>
-      }
     </div>
   );
 }
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,12 +1,33 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useContext } from "react";
 import styles from "./burger-ingredients.module.css";
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import IngredientList from "../ingredient-list/ingredient-list";
 import PropTypes from 'prop-types';
 import { menuItemPropTypes } from "../../utils/prop-types";
+import { BurgerContext } from "../../services/burger-context";
+import IngredientDetails from "../ingredient-details/ingredient-details";
+import Modal from "../modal/modal";
 
-const BurgerIngredients = (props) => {
+const BurgerIngredients = () => {
+
+   const data = useContext(BurgerContext)
    const [current, setCurrent] = React.useState('rolls')
+   const [isIngredientDetails, setIsIngredientDetails] = React.useState(false);
+   const [id, setId] = React.useState('')
+
+
+   const closeIngredientModal = () => {
+      setIsIngredientDetails(false)
+   };
+
+   const openIngredientModal = (id) => {
+      setIsIngredientDetails(true)
+      setId(id)
+   }
+
+   const handleEscKeydown = (e) => {
+      e.key === "Escape" && closeIngredientModal();
+   };
 
    const rollsRef = useRef(null)
    const saucesRef = useRef(null)
@@ -27,26 +48,37 @@ const BurgerIngredients = (props) => {
    }, [current])
 
    return (
-      <section className={styles.section}>
-         <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
-         <div className={styles.tabs}>
-            <div className={styles.tab}>
-               <Tab value="rolls" active={current === 'rolls'} onClick={setCurrent}>Булки</Tab>
+      <>
+         <section className={styles.section}>
+            <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
+            <div className={styles.tabs}>
+               <div className={styles.tab}>
+                  <Tab value="rolls" active={current === 'rolls'} onClick={setCurrent}>Булки</Tab>
+               </div>
+               <div className={styles.tab}>
+                  <Tab value="sauces" active={current === 'sauces'} onClick={setCurrent}>Соусы</Tab>
+               </div>
+               <div className={styles.tab}>
+                  <Tab value="toppings" active={current === 'toppings'} onClick={setCurrent}>Начинки</Tab>
+               </div>
             </div>
-            <div className={styles.tab}>
-               <Tab value="sauces" active={current === 'sauces'} onClick={setCurrent}>Соусы</Tab>
+            <div className={styles.ingredients} id='ingredients'>
+               <IngredientList id='rolls' name='Булки' type='bun' ref={rollsRef} openIngredientModal={openIngredientModal} />
+               <IngredientList id='sauces' name='Соусы' type='sauce' ref={saucesRef} openIngredientModal={openIngredientModal} />
+               <IngredientList id='toppings' name='Начинки' type='main' ref={toppingsRef} openIngredientModal={openIngredientModal} />
             </div>
-            <div className={styles.tab}>
-               <Tab value="toppings" active={current === 'toppings'} onClick={setCurrent}>Начинки</Tab>
-            </div>
-         </div>
-         <div className={styles.ingredients} id='ingredients'>
-            <IngredientList id='rolls' name='Булки' type='bun' ref={rollsRef} openIngredientModal={props.openIngredientModal} />
-            <IngredientList id='sauces' name='Соусы' type='sauce' ref={saucesRef} openIngredientModal={props.openIngredientModal} />
-            <IngredientList id='toppings' name='Начинки' type='main' ref={toppingsRef} openIngredientModal={props.openIngredientModal} />
-         </div>
-      </section>
+         </section>
 
+         {isIngredientDetails &&
+            <Modal
+               title="Детали ингредиента"
+               onOverlayClick={closeIngredientModal}
+               onEscKeydown={handleEscKeydown}
+            >
+               <IngredientDetails data={data} id={id} />
+            </Modal>
+         }
+      </>
    )
 }
 
@@ -54,5 +86,5 @@ export default BurgerIngredients
 
 
 BurgerIngredients.propTypes = {
-   openIngredientModal: PropTypes.func.isRequired
+
 }
