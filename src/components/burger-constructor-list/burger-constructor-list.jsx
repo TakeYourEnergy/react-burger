4f818f899e89a60d4ce1babf30b650c7440e043c
Alexander <sashashvetsov3@gmail.com>
2022-07-06 20:50:07 +0300

    add mains drag + add sum + add postOrder

--- src/components/burger-constructor-list/burger-constructor-list.jsx
+++ src/components/burger-constructor-list/burger-constructor-list.jsx
@@ -1,22 +1,30 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import styles from './burger-constructor-list.module.css';
 import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 import { menuItemPropTypes } from '../../utils/prop-types';
 import { useSelector } from 'react-redux';
+import { useDrop, useDrag } from "react-dnd";
 
-const BurgerConstructorList = () => {
+const BurgerConstructorList = ({ item, index }) => {
    // const data = useSelector(state => state.ingredientsReducer.ingredients)
    // const bun = { ...data[0] }
 
+   const [, drop] = useDrop({
+      accept: 'item',
+      hover(item, monitor) {
+         console.log(item)
+      }
+   })
+
    return (
 
-      <div className={styles.main} >
+      <div ref={drop} className={styles.main} >
          <DragIcon type="main" />
          <ConstructorElement
-            text="dsadasd"
-            price={50}
-         //thumbnail={item.image_mobile}
+            text={item.name}
+            price={item.price}
+            thumbnail={item.image}
          />
       </div>
 
--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,5 +1,4 @@
 import { useMemo, useState } from "react";
-import BurgerConstructorList from "../burger-constructor-list/burger-constructor-list";
 import styles from './burger-constructor.module.css';
 import { Button, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import Modal from "../modal/modal";
@@ -11,23 +10,17 @@ import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-comp
 import { useDrop } from "react-dnd";
 import { v4 as uuidv4 } from 'uuid';
 import { ADD_ITEM } from "../../services/actions/burger-constructor";
+import BurgerConstructorList from "../burger-constructor-list/burger-constructor-list";
 
 
 const BurgerConstructor = () => {
+   const dispatch = useDispatch()
+   const [totalPrice, setTotalPrice] = useState(0)
 
    const buns = useSelector(state => state.burgerConstructorReducer.buns)
    const mains = useSelector(state => state.burgerConstructorReducer.mains)
-   console.log(mains)
 
 
-
-   const data = useSelector(state => state.ingredientsReducer.ingredients)
-   const bunId = useSelector(state => state.ingredientsReducer.ingredients.find(item => item.name === 'Краторная булка N-200i'))
-   //console.log('data>>>', data)
-
-   const [totalPrice, setTotalPrice] = useState(0)
-
-   const dispatch = useDispatch()
    const orderLoading = useSelector(state => state.orderReducer.ingrSpin)
    const isOrderDetailsOpened = useSelector(state => state.orderReducer.isOrderDetailsOpened)
 
@@ -41,30 +34,26 @@ const BurgerConstructor = () => {
       },
    });
 
-
+   //вычисление суммы заказа
    useMemo(() => {
-      let sum = data.reduce((acc, item) => {
-         return item.type !== 'bun'
-            ? acc += item.price
-            : item.name === 'Краторная булка N-200i' ? acc += 2 * item.price : acc
-      }, 0)
-      setTotalPrice(sum)
-   }, [data])
-
-   const dataId = data.map(item => item._id) //без одной булки
-   //console.log('dataID>>>', dataId)
+      let sumMains = mains.reduce((acc, item) => acc + item.price, 0)
+      let sumBuns = buns.price ? buns.price * 2 : 0
+      setTotalPrice(sumMains + sumBuns)
+   }, [mains, buns])
 
 
    const postOrder = () => {
-      if ({ ...bunId }._id !== 'undefined') {
-         const orderArrIdAll = [...dataId, { ...bunId }._id]
+      if (mains.length && buns.price > 0) {
+         const orderArrIdAll = [...mains, buns, buns]
          dispatch(getOrderNumber(orderArrIdAll))
+      } else {
+         alert('не хватает либо булки, либо нет ингридиентов')
       }
    }
 
    return (
       <>
-         <section className={styles.section}>
+         <section ref={dropTarget} className={styles.section}>
             <div className={styles.box}>
                <div className={`${styles.bun}`}>
                   {buns.type && <ConstructorElement
@@ -77,12 +66,14 @@ const BurgerConstructor = () => {
                </div>
 
                <div ref={dropTarget} className={styles.item}>
-                  <div>
-                     <BurgerConstructorList />
-                  </div>
+                  {mains.map((item, index) =>
+                     <div key={item.uuid}>
+                        <BurgerConstructorList item={item} index={index} />
+                     </div>
+                  )}
                </div>
 
-               <div className={`${styles.bun}`}>
+               <div ref={dropTarget} className={`${styles.bun}`}>
                   {buns.type && <ConstructorElement
                      type="bottom"
                      isLocked={true}
